ade5beff18fcd85ba472715ed33695aabec128ac
ahmadabdul592 <ahmadabdul592@gmail.com>
2023-01-25 15:26:01 +0000

    Log Parsing

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,42 +1,33 @@
 #!/usr/bin/python3
-"""
-This module contains the function that displays the
-stats from the standard input
-"""
-import re
+'''a script that reads stdin line by line and computes metrics'''
+
+
 import sys
+
+# Initialize variables to store metrics
+total_size = 0
 status_codes = {200: 0, 301: 0, 400: 0, 401: 0,
                 403: 0, 404: 0, 405: 0, 500: 0}
-print_counter = 0
-size_summation = 0
+line_count = 0
 
 
-def print_logs():
-    """
-    Prints status codes to the logs
-    """
-    print("File size: {}".format(size_summation))
-    for k, v in sorted(status_codes.items()):
-        if v != 0:
-            print("{}: {}".format(k, v))
-
-
-if __name__ == "__main__":
-    try:
-        for line in sys.stdin:
-            std_line = line.replace("\n", "")
-            log_list = re.split('- | "|" | " " ', str(std_line))
-            try:
-                codes = log_list[-1].split(" ")
-                if int(codes[0]) not in status_codes.keys():
-                    continue
-                status_codes[int(codes[0])] += 1
-                print_counter += 1
-                size_summation += int(codes[1])
-                if print_counter % 10 == 0:
-                    print_logs()
-            except():
-                pass
-        print_logs()
-    except KeyboardInterrupt:
-        print_logs()
+# Read input line by line
+for line in sys.stdin:
+    # Check if line matches the specified format
+    parts = line.split()
+    if len(parts) != 6:
+        continue
+    ip, date, request, status, code, size = parts
+    status_code = int(code)
+    # Check if status code is in the list of possible codes
+    if status_code not in status_codes:
+        continue
+    # Update metrics
+    total_size += int(size)
+    status_codes[status_code] += 1
+    line_count += 1
+    # Print metrics every 10 lines
+    if line_count % 10 == 0:
+        print("Total file size: ", total_size)
+        for code in sorted(status_codes):
+            print(f"{code}: {status_codes[code]}")
